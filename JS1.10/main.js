da637289a2fa379a11f14bd86fb774777b0adb19
bla84diamond <bla84diamond@gmail.com>
2023-05-25 19:21:34 +0300

    .

--- JS1.10/main.js
+++ JS1.10/main.js
@@ -4,6 +4,6 @@ const passwordInput = document.querySelector('#password');
 form.addEventListener('submit', (event) => {
   if (passwordInput.value.length < 8) {
     event.preventDefault();
-    alert('Password must contain at least 8 characters');
+    alert('Пароль должен состоять минимум из 8 символов!');
   }
 });
---
+++ JS4.1/main.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const e = React.createElemnt;
+
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked : false};
+    }
+    render() {
+        if (this.state.liked) {
+            return 'U like'
+        }
+        return e(
+            'button',
+            {onClick: () => this.setState({ liked:true})},
+            'Like'
+            );
+    }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer)
